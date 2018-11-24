2b1df439f51807ac38abe65c0d7490696a7a428d
Kalesh Singh <kaleshwar.singh@bison.howard.edu>
2018-11-23 19:07:02 -0800

    Layout components

--- app/src/main/java/com/techexchange/mobileapps/wifidirectlab/MainActivity.java
+++ app/src/main/java/com/techexchange/mobileapps/wifidirectlab/MainActivity.java
@@ -2,12 +2,35 @@ package com.techexchange.mobileapps.wifidirectlab;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button onOffButton;
+    Button discoverButton;
+    Button sendButton;
+    ListView listView;
+    TextView readMsgTextView;
+    TextView connectionStatusTextView;
+    EditText writeMsgEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initializeComponents();
+    }
+
+    private void initializeComponents() {
+        onOffButton = findViewById(R.id.onOff);
+        discoverButton = findViewById(R.id.discover);
+        sendButton = findViewById(R.id.sendButton);
+        listView = findViewById(R.id.peerListView);
+        readMsgTextView = findViewById(R.id.readMsg);
+        connectionStatusTextView = findViewById(R.id.connectionStatus);
+        writeMsgEditText = findViewById(R.id.writeMsg);
     }
 }
