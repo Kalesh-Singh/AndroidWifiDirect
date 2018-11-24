afa29030df716e12ef746b3a8e2b137f7c9ad48a
Kalesh Singh <kaleshwar.singh@bison.howard.edu>
2018-11-23 19:20:59 -0800

    Toggle wifi on / off

--- app/src/main/java/com/techexchange/mobileapps/wifidirectlab/MainActivity.java
+++ app/src/main/java/com/techexchange/mobileapps/wifidirectlab/MainActivity.java
@@ -1,5 +1,7 @@
 package com.techexchange.mobileapps.wifidirectlab;
 
+import android.content.Context;
+import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -17,11 +19,26 @@ public class MainActivity extends AppCompatActivity {
     TextView connectionStatusTextView;
     EditText writeMsgEditText;
 
+    WifiManager wifiManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initializeComponents();
+        executeListener();
+    }
+
+    private void executeListener() {
+        onOffButton.setOnClickListener(v -> {
+            if (wifiManager.isWifiEnabled()) {
+                wifiManager.setWifiEnabled(false);
+                onOffButton.setText("Wifi On");
+            } else {
+                wifiManager.setWifiEnabled(true);
+                onOffButton.setText("Wifi off");
+            }
+        });
     }
 
     private void initializeComponents() {
@@ -32,5 +49,10 @@ public class MainActivity extends AppCompatActivity {
         readMsgTextView = findViewById(R.id.readMsg);
         connectionStatusTextView = findViewById(R.id.connectionStatus);
         writeMsgEditText = findViewById(R.id.writeMsg);
+
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        if (wifiManager.isWifiEnabled()) {
+            onOffButton.setText("Wifi off");
+        }
     }
 }
